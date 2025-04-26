4f9df0506b30d46523c49cd47620c313e09108df
Aaron <200678797+OtterLover6000@users.noreply.github.com>
2025-04-25 23:31:29 -0700

    Complete assignment

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 // Do not change this file.
 // Use q1.hpp file to complete the functions
 
-int main()
-{
-	int begin, end;
-	int prime1, prime2;
+int main() {
+    int begin, end;
 
-	getTwoValues(begin, end);
-	prime1 = getNextPrime(begin);
-	cout << "The prime number after " << begin << " is " << prime1 << endl;
-	prime2 = getPrevPrime(end);
-	cout << "The prime number before " << end << " is " << prime2 << endl;
+    getTwoValues(begin, end);
+    int prime1 = getNextPrime(begin);
+    cout << "The prime number after " << begin << " is " << prime1 << endl;
+    int prime2 = getPrevPrime(end);
+    cout << "The prime number before " << end << " is " << prime2 << endl;
 }
--- main.hpp
+++ main.hpp
@@ -1,8 +1,46 @@
+#pragma once
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+inline void getTwoValues(int &begin, int &end) {
+    int possibleBegin, possibleEnd;
+    cin >> possibleBegin >> possibleEnd;
+    while (possibleBegin >= possibleEnd) {
+        cout << "Please enter two integers where the first is less than the second: ";
+        cin >> possibleBegin >> possibleEnd;
+    }
+    begin = possibleBegin;
+    end = possibleEnd;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+inline bool isPrime(int begin) {
+    bool isPrime = true;
+    for (int i = 2; i <= begin / 2; i++) {
+        if (begin % i == 0) {
+            isPrime = false;
+        }
+    }
+    return isPrime;
+}
+
+inline int getNextPrime(int begin) {
+    if (isPrime(begin)) {
+        begin++;
+    }
+    while (!isPrime(begin)) {
+        begin++;
+    }
+    return begin;
+}
+
+
+inline int getPrevPrime(int end) {
+    if (isPrime(end)) {
+        end--;
+    }
+
+    while (!isPrime(end)) {
+        end--;
+    }
+    return end;
+}
